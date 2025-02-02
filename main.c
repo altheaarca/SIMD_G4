15fec7f934ac8edd3e5d514e87368b5075b749bc
altheaarca <althea_arca@dlsu.edu.ph>
2025-02-03 02:23:49 +0800

    checker()

--- main.c
+++ main.c
@@ -1,53 +1,36 @@
-//Group 4 : Arca, Co Chiong, Uy [S11]
-// NON - SIMD assembly language
+// Group 4 : Arca, Co Chiong, Uy [S11]
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
 #include <time.h>
 #include "kernel.h"
-
-//TODO: Fix warnings. Function Definition for these three are not found.
-extern void x86(int n,double* x, double* y);
-extern void stencil_1D_xmm_SIMD(int n, double* X, double* Y);
-extern void stencil_1D_ymm_SIMD(int n, double* X, double* Y);
-
 int main() {
-    int vLength, i;
-    double pcFreq;          // Counter frequency 
-    __int64 counterStart;   // Timer value
-    LARGE_INTEGER li;       // Large integer for timer value
-    double time_taken;      // Time taken in seconds
-    double average_time = 0;// Average time
-    int retcode;            // Return code
+    int vLength = 8; //TODO:  replace : 1 << 20 
+    int i;
+    double time;
+    clock_t start, end;
+    double* answer = (double*)malloc(vLength * sizeof(double));
 
-    // frequency of counter
-    retcode = QueryPerformanceFrequency(&li);
-    if (retcode == 0)
-        printf("ERROR: QueryPerformanceFrequency() failed\n");
-    pcFreq = li.QuadPart;
-
-    // Get the vector length
-    printf("Enter the length of vector X: ");
-    if (scanf_s("%d", &vLength) != 1 || vLength < 4) {
-        printf("Invalid input. Vector length must be at least 4.\n");
+    if (!answer) {
+        printf("Memory allocation for answer failed.\n");
         return 1;
     }
-    // double precision memory alloc
+
+    // Memory allocation for vectors X and Y
     double* X = (double*)malloc(vLength * sizeof(double));
     double* Y = (double*)malloc(vLength * sizeof(double));
-    double* answer = (double*)malloc(vLength * sizeof(double));
-    if (!X || !Y || !answer) {
+    if (!X || !Y) {
         printf("Memory allocation failed.\n");
         free(X);
         free(Y);
         free(answer);
         return 1;
     }
-    // X elements 
+
+    // Array initialization
     printf("Enter %d elements for vector X:\n", vLength);
     for (i = 0; i < vLength; i++) {
         if (scanf_s("%lf", &X[i]) != 1) {
-            printf("Invalid input. Please enter numeric values.\n");
+            printf("Invalid input.\n");
             free(X);
             free(Y);
             free(answer);
@@ -55,21 +38,44 @@ int main() {
         }
     }
 
-    // C Version 
-    compute_kernel(X, answer, vLength);
-    QueryPerformanceCounter(&li); //start time 
-    counterStart = li.QuadPart;
+    // Time measurement for C version
+    start = clock();
     compute_kernel(X, Y, vLength);
-    QueryPerformanceCounter(&li); //end time
-    time_taken = (double)(li.QuadPart - counterStart) / pcFreq;
-    printf("Time taken for compute_kernel (C version): %lf seconds\n", time_taken);
-    average_time = average_time + time_taken;
-    // Display result after C kernel
+    end = clock();
+    time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Time taken for compute_kernel: %g seconds\n", time);
+    display_vector(Y, vLength);
+
+    // Non-SIMD Assembly Kernel
+    printf("\n[Running Non-SIMD Assembly Kernel]\n");
+    start = clock();
+    x86(vLength, X, Y);
+    end = clock();
+    time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Time taken for x86 (Non-SIMD): %.6lf seconds\n", time);
+    display_vector(Y, vLength);
+
+    // SIMD XMM Kernel
+    printf("\n[Running SIMD Assembly Kernel XMM Register]\n");
+    start = clock();
+    stencil_1D_xmm_SIMD(vLength, X, Y);
+    end = clock();
+    time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Time taken for SIMD XMM: %.6lf seconds\n", time);
+    display_vector(Y, vLength);
+
+    // SIMD YMM Kernel
+    printf("\n[Running SIMD Assembly Kernel YMM Register]\n");
+    start = clock();
+    stencil_1D_ymm_SIMD(vLength, X, Y);
+    end = clock();
+    time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Time taken for SIMD YMM: %.6lf seconds\n", time);
     display_vector(Y, vLength);
 
     // Run and display results for assembly kernels
     printf("\n[Running Non-SIMD Assembly Kernel]\n");
-    x86(X, Y, vLength);
+    x86(vLength, X, Y);
     display_vector(Y, vLength);
     if (compare_vectors(Y, answer, vLength, 1e-6)) {
         printf("Non-SIMD assembly kernel result matches C reference!\n");
@@ -99,10 +105,9 @@ int main() {
     }
 
     // Free allocated memory
-    free(X);
+    //free(X);
     free(Y);
-    free(answer);
+    //free(answer);
 
     return 0;
 }
-
